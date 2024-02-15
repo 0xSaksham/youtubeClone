10dcd7bf528bb93a60fedebe333d3eb767abb5d1
Saksham Gupta <sakshamgupta.ai@gmail.com>
2024-02-15 20:00:01 +0530

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -5,4 +5,12 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server Running on Port ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO DB Connection Failed! ", err);
+  });
